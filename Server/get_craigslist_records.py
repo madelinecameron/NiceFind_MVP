65b1411733cd2d6a5463e5384d87985f39238e40
Madeline Cameron <madeline@madelinecameron.net>
2015-02-24 00:48:19 -0600

    Fixed script for populating database with items Updated home template to look prettier!

--- Server/get_craigslist_records.py
+++ Server/get_craigslist_records.py
@@ -1,21 +1,20 @@
 import sys, os, requests, json
 from pymongo import MongoClient
-from parse_json_v1 import parse_items
-
-global ANCHOR
-ANCHOR = None
 
 def get_records(api_key):
 	next_page = 1
 	params = { 'auth_token' : api_key, 'status' : 'for_sale', 'lat' : 37.956002, 'long' : -91.774363, 'radius' : '100mi', 'category': 'SANT|SCOL|SJWL', 'has_image': 1, 
 	'has_price': 1, 'rpp': 100, 'retvals': 'external_id,category,heading,location,body,timestamp,price,images' }
 	
+	anchor = None
+	external_id_set = set()
+	item_list = []
 	while(next_page > 0):
 		request_url = None
-		if ANCHOR is None:
+		if anchor is None:
 			request_url = "http://search.3taps.com/?" + '&'.join("{!s}={!s}".format(key, val) for (key, val) in params.items())
 		else:
-			params.update({'anchor': ANCHOR})
+			params.update({'anchor': anchor})
 			request_url = "http://polling.3taps.com/poll?" + '&'.join("{!s}={!s}".format(key, val) for (key, val) in params.items())
 			
 		response = requests.get(request_url)
@@ -23,8 +22,7 @@ def get_records(api_key):
 		content_decode = response.content.decode("utf-8")
 		json_parse = json.loads(content_decode)
 
-		external_id_set = set()
-		item_list = []
+		print(json_parse)
 		for item in json_parse["postings"]:
 			if(item["external_id"] not in external_id_set):
 				geojson_obj = {
@@ -32,14 +30,13 @@ def get_records(api_key):
 						'coordinates': [ float(item["location"]["long"]), float(item["location"]["lat"]) ]
 					}
 				external_id_set.add(item["external_id"])
-				parsed_json = parse_items({ 'name': item["heading"], 'price': item["price"], 'image_urls': item["images"], 'body': item["body"], 'loc': geojson_obj, 'timestamp': item["timestamp"]})
-				item_list.append(parsed_json)
+				item_list.append({ 'name': item["heading"], 'price': item["price"], 'image_urls': item["images"], 'body': item["body"], 'loc': geojson_obj, 'timestamp': item["timestamp"]})
 			
 		next_page = json_parse['next_page']
 		params = { 'auth_token' : api_key, 'status' : 'for_sale', 'lat' : 37.956002, 'long' : -91.774363, 'radius' : '100mi', 'category': 'SANT', 'has_image': 1, 
 			'has_price': 1, 'rpp': 100, 'retvals': 'external_id,category,heading,location,body,timestamp,price,images', 'page': next_page}
 	
-	ANCHOR = item["anchor"]
+	anchor = json_parse["anchor"]
 	return item_list
 	
 def put_records_in_db(records, user, password):
@@ -52,11 +49,18 @@ def put_records_in_db(records, user, password):
 		print("Inserted: %s" % inserted_item)
 		
 if __name__ == "__main__":
+	global anchor
+	anchor = None
+
 	with open("./three_taps_conf") as conf:
 		api_key = conf.readline().replace("\n", "")
 		db_user = conf.readline().replace("\n", "")
 		db_pass = conf.readline().replace("\n", "")
 	
 	records = get_records(api_key)
+	with open('dump.json', 'w+') as dump_file:
+		dump_file.write(json.dumps(records, sort_keys=True,
+                  indent=4, separators=(',', ': ')))
+		sys.exit(0)
 	put_records_in_db(records, db_user, db_pass)
 	
