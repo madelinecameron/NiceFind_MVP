626a1163550498c9382de1c87f1a61cb89be084e
Madeline Cameron <madeline@madelinecameron.net>
2015-03-08 17:22:26 -0500

    Front-end changes Item-detail has multiple pictures now homeCntrl's request URL was changed to refactorered server's call Town in header is now auto-populated by where the user is Radius in header can be changed (will be implemented later with search)

--- Front-End/www/js/controllers.js
+++ Front-End/www/js/controllers.js
@@ -1,16 +1,29 @@
 'use strict';
 angular.module('solobuy.controllers', [])
-
 .controller('homeCntrl', function($scope, $state, $http, $q) {
 	navigator.geolocation.getCurrentPosition(function(position) {
-			$http.get('http://104.236.44.62:3000/dashboard?lat=' + position.coords.latitude + '&long=' + position.coords.longitude).
+		//$http.defaults.headers.get = { 'If-Checked-Since' : new Date().getTime() };
+		$http.get('http://104.236.44.62:3000/search?lat=' + position.coords.latitude + '&long=' + position.coords.longitude).
+			success(function(data, status, headers, config) {
+				$scope.items = data;
+			}).
+			error(function(data, status, headers, config) {
+				console.log("Failed");
+				console.dir(config);
+				console.log("Error:" + status);
+				return 0;
+			});
+
+			$http.get('http://104.236.44.62:3000/town?lat=' + position.coords.latitude + '&long=' + position.coords.longitude).
 				success(function(data, status, headers, config) {
-          console.dir(data);
-					$scope.items = data;
+					$scope.town = data.obj.name;
+					$scope.radius = 100;
 				}).
 				error(function(data, status, headers, config) {
 					console.log("Failed");
 					console.log("Error:" + status);
+					$scope.town = "NaN";
+					$scope.radius = 100;
 					return 0;
 				});
 		});
@@ -19,6 +32,7 @@ angular.module('solobuy.controllers', [])
   $http.get('http://104.236.44.62:3000/item/' + $stateParams.id).
     success(function(data, status, headers, config) {
       $scope.item = data[0];
+			$scope.imageList = data[0].image_urls;
     }).
     error(function(data, status, headers, config) {
       console.log("Failed");
@@ -33,9 +47,9 @@ angular.module('solobuy.controllers', [])
   console.log("I don't do anything! :D")
 })
 
-$scope.CallTel = function(tel) {
-            window.location.href = 'tel:'+ tel;
-}
+//$scope.CallTel = function(tel) {
+//            window.location.href = 'tel:'+ tel;
+//}
 //imgslide.controller ('ImgSlideCntrl', function(scope) {
 //    $scope.imgSlide = function(index) {
 //    $ionicSlideBoxDelegate.slide(index, 500);
