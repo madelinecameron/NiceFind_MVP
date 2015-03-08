3ff52189fa6a5c7db833ad6882f8df1bf44f2ef7
Nick Rollins <nickallenrollins@gmail.com>
2015-03-08 12:25:00 -0500

    Added categories back to search tab

--- Front-End/www/js/controllers.js
+++ Front-End/www/js/controllers.js
@@ -35,59 +35,9 @@ angular.module('solobuy.controllers', [])
 
 $scope.CallTel = function(tel) {
             window.location.href = 'tel:'+ tel;
-};
-
-//$scope.init = function(){
-//    $scope.page = 1;
-//    $scope.getData()
-//    .then(function(res){
-//      // success
-//      console.log('Data: ', res)
-//      $scope.dataList = res.shots;
-//    }, function(status){
-//      // err
-//      $scope.pageError = status;
-//    })
-//  }
-//
-//  $scope.setActive = function(index){
-//    angular.forEach($scope.dataList, function(data){
-//      data.active = false;
-//    })
-//
-//    $scope.dataList[index].active = true
-//  }
-//
-//  $scope.getData = function(){
-//    var defer = $q.defer();
-//
-//        $http.get('http://polling.3taps.com/poll?auth_token=aab1bc69478450008facde2d6824cb0b')
-//            .success(function(res) {
-//				defer.resolve(res)
-//
-//            })
-//
-//    .error(function(status, err){
-//      defer.reject(status)
-//    })
-//
-//    return defer.promise;
-//  }
-//
-//  $scope.nextPage = function(){
-//    $scope.page += 1;
-//
-//    $scope.getData()
-//    .then(function(res){
-//      if($scope.dataList[0]){
-//        $scope.dataList = $scope.dataList.concat(res.shots)
-//      }
-//      else{
-//        $scope.dataList = res.shots;
-//      }
-//      console.log('nextPage: ', $scope.dataList)
-//      $scope.$broadcast('scroll.infiniteScrollComplete');
-//    })
-//  }
-//
-//  $scope.init();
+}
+//imgslide.controller ('ImgSlideCntrl', function(scope) {
+//    $scope.imgSlide = function(index) {
+//    $ionicSlideBoxDelegate.slide(index, 500);
+//    }
+//});
